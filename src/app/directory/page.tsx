c94782dda84c7be91051b873676337eab9b905e4
nekowawolf <irukainu20@gmail.com>
2026-07-15 16:12:05 +0700

    feat: add nwwonee ai

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCommunity } from "@/hooks/useCommunity";
+import NwwOneeAIChat from "@/components/NwwOneeAIChat";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CommunityCard from "@/components/CommunityCard";
@@ -178,6 +179,7 @@ function CommunityContent() {
                 </div>
             </div>
             <Footer />
+            <NwwOneeAIChat />
         </>
     );
 }
