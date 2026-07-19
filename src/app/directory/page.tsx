217116cb08e9ea224b6656a7efd9da756bce44e1
nekowawolf <irukainu20@gmail.com>
2026-07-19 23:38:45 +0700

    style: update no data found image

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -149,15 +149,15 @@ function CommunityContent() {
 
                             <div id="fillcommunity" className="w-full flex-col flex gap-4">
                                 {communityData.length === 0 ? (
-                                    <div className="text-center py-10">
+                                    <div className="text-center py-1">
                                         <Image
-                                            src="https://nekowawolf.github.io/cdn-images/images/2026/1771661079_pixchan.png"
+                                            src="https://nekowawolf.github.io/cdn-images/images/2026/1784476217_nwwonee_search.webp"
                                             alt="No data found"
-                                            width={176}
-                                            height={176}
+                                            width={160}
+                                            height={160}
                                             className="mx-auto"
                                         />
-                                        <p className="text-gray-500 mt-4">No data available.</p>
+                                        <p className="text-fill-color/50 -mt-4">No data available.</p>
                                     </div>
                                 ) : (
                                     communityData.map((item) => (
